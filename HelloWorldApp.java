428dd533f0004181e119e230435ba500b6790c43
pulykaman <35999258+pulykaman@users.noreply.github.com>
2018-02-23 23:44:33 +0100

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,11 +1,11 @@
 class HelloWorldApp {
     public static void main(String[] args) {
 		String asd = "Asd";
-		if(asd.equals("Asd")){
+		if("Asd".equals(asd)){
 			 System.out.println("Hello World!");
 		}
 		else {
-			System.out.println("Bye World!");
+			System.out.println("Bye-Bye World!");
 		}    
     }
 }
