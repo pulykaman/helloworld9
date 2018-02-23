960928d1d54a92ab7ee07a20eb4297ef5c4ec45d
pulykaman <35999258+pulykaman@users.noreply.github.com>
2018-02-23 23:44:04 +0100

    Create HelloWorldApp.java

---
+++ HelloWorldApp.java
@@ -0,0 +1,11 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+		String asd = "Asd";
+		if(asd.equals("Asd")){
+			 System.out.println("Hello World!");
+		}
+		else {
+			System.out.println("Bye World!");
+		}    
+    }
+}
